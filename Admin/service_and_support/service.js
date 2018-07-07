3dc19ffb60172ad3d92ba37196dbd91ce82e412f
wdz <370756541@qq.com>
2018-07-07 15:35:15 +0800

    wdz

--- Admin/service_and_support/service.js
+++ Admin/service_and_support/service.js
@@ -1,4 +1,10 @@
 $(function () {
+    $(document).on('mouseenter','.summarize_cont ul li',function () {
+        $(this).find('.summarize_down').find('p:first-child').css('color','#41d1aa');
+    });
+    $(document).on('mouseleave','.summarize_cont ul li',function () {
+        $(this).find('.summarize_down').find('p:first-child').css('color','#333');
+    });
     // 方案简介上浮动画
     $(document).on('mouseenter','.case_cont ul li div',function () {
         let img_1 = $(this).find('img').attr('src').split('_')[0];
