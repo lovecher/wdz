e2156b395e5974524afa2c587c37d53218f4985a
wdz <370756541@qq.com>
2018-07-19 18:45:03 +0800

    wddz

--- Admin/about_us/about_us.js
+++ Admin/about_us/about_us.js
@@ -1,2 +1,13 @@
 $(function () {
-}
+    // 方案简介上浮动画
+    $(document).on('mouseenter','.case_cont ul li div',function () {
+        let img_1 = $(this).find('img').attr('src').split('_')[0];
+        console.log(img_1)
+        $(this).find('img').attr('src',img_1+'_2.png');
+    });
+    $(document).on('mouseleave','.case_cont ul li div',function () {
+        let img_1 = $(this).find('img').attr('src').split('_')[0];
+        console.log(img_1)
+        $(this).find('img').attr('src',img_1+'_1.png');
+    })
+})
